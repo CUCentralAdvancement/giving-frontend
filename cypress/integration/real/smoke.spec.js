159ff107781cebccfcc6d1d13bbff2b374c59065
Alex Finnarn <alexfinnarn@gmail.com>
2020-06-05 13:38:46 -0400

    add baseUrl and viewport Cypress variables

--- cypress/integration/real/smoke.spec.js
+++ cypress/integration/real/smoke.spec.js
@@ -1,17 +1,19 @@
 describe("Smoke Test Critical Paths", () => {
   it("navigates to the homepage/fund search", () => {
     cy.request({
-      url: "http://localhost:3000/",
+      url: "/",
       followRedirect: false, // turn off following redirects
     }).then((resp) => {
       // redirect status code is 302
       expect(resp.status).to.eq(302);
-      expect(resp.redirectedToUrl).to.eq("http://localhost:3000/fund-search");
+      expect(resp.redirectedToUrl).to.eq(
+        `${Cypress.config("baseUrl")}/fund-search`
+      );
     });
   });
 
   it("Loads fund search page with at least one result and navigated to fund", () => {
-    cy.visit("http://localhost:3000/fund-search");
+    cy.visit("/fund-search");
     cy.contains("Resident Emergency Relief Fund");
 
     // @todo Add navigation to fund.
@@ -19,7 +21,7 @@ describe("Smoke Test Critical Paths", () => {
 
   // it("Loads fund page", () => {
   //   // Need to get allocation route with replacement to fund name in path shown to users.
-  //   cy.visit("http://localhost:3000/fund/0123456");
+  //   cy.visit("/fund/0123456");
   //   cy.contains("Resident Emergency Relief Fund");
   // });
 });
