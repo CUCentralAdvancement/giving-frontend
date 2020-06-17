e39e8a45a181aee7f05eb6b24fbd3810b69c5b27
Alex Finnarn <alexfinnarn@gmail.com>
2020-06-17 10:45:02 -0400

    add utility hooks

---
+++ utils/hooks.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from "react";
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+const useWindowDimensions = () => {
+  if (typeof window === "undefined") {
+    return { width: 0, height: 0 };
+  }
+
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+};
+
+const useIntersectionObserver = ({
+  target,
+  onIntersect,
+  threshold = 0.1,
+  rootMargin = "0px",
+}) => {
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersect, {
+      rootMargin,
+      threshold,
+    });
+
+    const current = target.current;
+
+    observer.observe(current);
+
+    return () => {
+      observer.unobserve(current);
+    };
+  });
+};
+
+export { useWindowDimensions, useIntersectionObserver };
