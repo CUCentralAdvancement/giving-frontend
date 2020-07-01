1bfd29149e0316a10ce595500d4ede4ec9ec8675
Alex Finnarn <alexfinnarn@gmail.com>
2020-07-01 11:16:59 -0400

    adding HTTPS server

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const next = require("next");
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
 
+const httpsPort = 3443;
 const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -18,8 +22,17 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
-  });
+  if (dev) {
+    const options = {
+      key: fs.readFileSync("localhost.key"),
+      cert: fs.readFileSync("localhost.crt"),
+    };
+    http.createServer(server).listen(port);
+    https.createServer(options, server).listen(httpsPort);
+  } else {
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  }
 });
