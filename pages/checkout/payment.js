dba77d0469cc4b8b102526c620d2d5d4fdd357c8
Alex Finnarn <alexfinnarn@gmail.com>
2020-07-01 11:19:17 -0400

    add authorize code to payment page

--- pages/checkout/payment.js
+++ pages/checkout/payment.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Link from "next/link";
 import Layout from "../../components/global/Layout";
 import {
@@ -10,7 +10,12 @@ import {
   Box,
 } from "@cu-advancement/component-library";
 
-export default function Payment() {
+export default function Payment({ token }) {
+  useEffect(() => {
+    if (typeof document !== "undefined") {
+      document.querySelector("#send_token").submit();
+    }
+  }, []);
   return (
     <Layout>
       <Flex
@@ -36,9 +41,23 @@ export default function Payment() {
           />
           <Text> Please do not use Refresh or Back buttons on this page.</Text>
         </Flex>
-        <Heading sx={{ mt: 4, textAlign: "center" }}>
-          Authorize.net Form Goes Here
-        </Heading>
+        <div
+          id="iframe_holder"
+          className="center-block"
+          style={{ width: "90%", maxWidth: "1000px", height: "600px" }}
+        >
+          <iframe
+            id="add_payment"
+            className="embed-responsive-item panel"
+            name="add_payment"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            scrolling="no"
+            // hidden="true"
+            title="Authorize.net Payment Form"
+          ></iframe>
+        </div>
         <Image
           src="https://giving-test.cu.edu/sites/all/themes/themekit/images/verified-auth-net.png"
           sx={{ width: "135px", mx: "auto" }}
@@ -58,6 +77,59 @@ export default function Payment() {
           </Link>
         </Box>
       </Flex>
+      <form
+        id="send_token"
+        action="https://test.authorize.net/payment/payment"
+        method="post"
+        target="add_payment"
+      >
+        <input type="hidden" name="token" value={token} />
+      </form>
+      {(function () {
+        if (typeof window !== "undefined" && !window.AuthorizeNetIFrame) {
+          window.AuthorizeNetIFrame = {};
+          AuthorizeNetIFrame.onReceiveCommunication = function (querystr) {
+            var params = parseQueryString(querystr);
+            switch (params["action"]) {
+              case "successfulSave":
+                break;
+              case "cancel":
+                break;
+              case "resizeWindow":
+                var w = parseInt(params["width"]);
+                var h = parseInt(params["height"]);
+                var ifrm = document.getElementById("add_payment");
+                ifrm.style.width = w.toString() + "px";
+                ifrm.style.height = h.toString() + "px";
+                break;
+              case "transactResponse":
+                var ifrm = document.getElementById("add_payment");
+                ifrm.style.display = "none";
+            }
+          };
+        }
+
+        function parseQueryString(str) {
+          var vars = [];
+          var arr = str.split("&");
+          var pair;
+          for (var i = 0; i < arr.length; i++) {
+            pair = arr[i].split("=");
+            vars.push(pair[0]);
+            vars[pair[0]] = unescape(pair[1]);
+          }
+          return vars;
+        }
+      })()}
     </Layout>
   );
+}
+
+export async function getServerSideProps({}) {
+  // Fetch data from external API
+  const res = await fetch("http://localhost:3000/api/authorize-token");
+  const token = await res.json();
+
+  // Pass data to the page via props
+  return { props: { token: token.token } };
 }
