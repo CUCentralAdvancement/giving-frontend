cebd149627b1d9d5a3c55b00f23bf8390998893c
Alex Finnarn <alexfinnarn@gmail.com>
2020-09-08 17:04:51 -0400

    fix eslint in search form

--- components/fund-search/SearchForm.js
+++ components/fund-search/SearchForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import {
   Flex,
   Box,
@@ -85,7 +85,11 @@ export default function SearchForm() {
   );
 }
 
-function CustomSearch({ currentRefinement, isSearchStalled, refine }) {
+CustomSearch.propTypes = {
+  refine: PropTypes.func,
+};
+
+function CustomSearch({ refine }) {
   return (
     <TextInput
       name="search"
