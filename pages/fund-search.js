ef5f68b6bbef275b8ec2be2265bd4da5856578b6
Alex Finnarn <alex.finnarn@gmail.com>
2021-03-24 19:58:19 -0500

    don't render search to see if build succeeds

--- pages/fund-search.js
+++ pages/fund-search.js
@@ -3,21 +3,21 @@ import React from "react";
 // import PropTypes from "prop-types";
 import { Box, Flex } from "@cu-advancement/component-library";
 import Layout from "../components/global/Layout";
-import SearchForm from "../components/fund-search/SearchForm";
-import SearchResults from "../components/fund-search/SearchResults";
-import algoliasearch from "algoliasearch/lite";
-import { InstantSearch, PoweredBy } from "react-instantsearch-dom";
+// import SearchForm from "../components/fund-search/SearchForm";
+// import SearchResults from "../components/fund-search/SearchResults";
+// import algoliasearch from "algoliasearch/lite";
+// import { InstantSearch, PoweredBy } from "react-instantsearch-dom";
 import { motion, AnimatePresence } from "framer-motion";
 
-const searchClient = algoliasearch(
-  process.env.NEXT_PUBLIC_ALGOLIA_ID,
-  process.env.NEXT_PUBLIC_ALGOLIA_KEY
-);
+// const searchClient = algoliasearch(
+//   process.env.NEXT_PUBLIC_ALGOLIA_ID,
+//   process.env.NEXT_PUBLIC_ALGOLIA_KEY
+// );
 
 export default function FundSearch() {
   return (
     <Layout>
-      <InstantSearch searchClient={searchClient} indexName="Funds">
+      {/* <InstantSearch searchClient={searchClient} indexName="Funds">
         <Flex sx={{ flexDirection: "column" }}>
           <Box sx={{ bg: "gray", width: "100%" }}>
             <Box sx={{ p: 3, maxWidth: "960px", mx: "auto" }}>
@@ -43,7 +43,7 @@ export default function FundSearch() {
             </AnimatePresence>
           </Box>
         </Flex>
-      </InstantSearch>
+      </InstantSearch> */}
     </Layout>
   );
 }
