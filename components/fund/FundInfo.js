87b6273c737f9341625437d3b0ea24db191de0dd
Alex Finnarn <alex.finnarn@gmail.com>
2021-04-05 17:08:57 -0400

    update fund slug for new backend (#419)

--- components/fund/FundInfo.js
+++ components/fund/FundInfo.js
@@ -1,71 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Link from "next/link";
-import { campusNames, interestsOptions } from "../../data/fundMeta";
-import {
-  Box,
-  Flex,
-  Heading,
-  Text,
-  theme,
-} from "@cu-advancement/component-library";
-
 export default function FundInfo({ fund }) {
   return (
-    <Flex sx={{ p: 4, flexDirection: "column" }}>
-      <Heading as="h1" sx={{ pb: 3 }} data-testid="fund-title">
+    <div className="flex flex-col p-4 h-full">
+      <h1 className="pb-3 text-2xl" data-testid="fund-title">
         {fund.title}
-      </Heading>
-      <Box sx={{ flex: "1 1 auto" }}>
-        <Text as="p" data-testid="fund-description">
-          {fund.description}
-        </Text>
-        <div
-          data-testid="fund-marketing-content"
-          dangerouslySetInnerHTML={{ __html: fund.marketingContent }}
-        ></div>
-      </Box>
-      <Box sx={{ pt: 2 }}>
-        <Text sx={{ display: "inline-block" }}>Campus:&nbsp;</Text>
+      </h1>
+      <div className="flex-grow">
+        <p data-testid="fund-description">{fund.description}</p>
+        <div data-testid="fund-marketing-content" dangerouslySetInnerHTML={{ __html: fund.marketingContent }}></div>
+      </div>
+      <div className="pt-2">
+        <span className="inline-block">Campus:&nbsp;</span>
         <Link href={`/fund-search?field_campus=${fund.campus}`}>
-          <a
-            data-testid="fund-campus"
-            style={{
-              color: theme.colors.primary,
-            }}
-          >
-            {campusNames[fund.campus]}
+          <a className="text-gold" data-testid="fund-campus">
+            {fund.campus}
           </a>
         </Link>
         <br />
-        <Text sx={{ display: "inline-block" }}>Allocation Code:&nbsp;</Text>
-        <Text
-          data-testid="fund-allocation-code"
-          sx={{ color: "primary", display: "inline-block" }}
-        >
-          {fund.allocationCode}
-        </Text>
+        <span className="inline-block">Allocation Code:&nbsp;</span>
+        <span className="inline-block text-gold" data-testid="fund-allocation-code">
+          {fund.allocation_code}
+        </span>
         <br />
-        <Text sx={{ display: "inline-block" }}>Interest:&nbsp;</Text>
+        <span className="inline-block">Interest:&nbsp;</span>
         <Link href={`/fund-search?field_fund_interests=${fund.interest}`}>
-          <a
-            data-testid="fund-interest"
-            style={{ color: theme.colors.primary }}
-          >
-            {(() => {
-              const inter = interestsOptions.find(
-                (el) => el.value === fund.interest
-              );
-              if (inter) {
-                return inter.label;
-              } else {
-                return fund.interest;
-              }
-            })()}
+          <a data-testid="fund-interest" className="text-gold">
+            {fund.interest}
           </a>
         </Link>
-      </Box>
-    </Flex>
+      </div>
+    </div>
   );
 }
 
