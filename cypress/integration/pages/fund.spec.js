9b4f453a742e7da65f70b1d2b245f14d6be3717a
Alex Finnarn <alex.finnarn@gmail.com>
2021-06-04 10:14:03 -0500

    add two tests for license plate and write-in funds

--- cypress/integration/pages/fund.spec.js
+++ cypress/integration/pages/fund.spec.js
@@ -15,11 +15,53 @@ describe("Fund Detail Page Tests", () => {
     cy.get('[data-testid="fund-allocation-code"]').contains('0430106');
     cy.get('[data-testid="fund-interest"]').contains('Scholarships & Student Success');
 
+    // Should not have license plate form field.
+    cy.get('input[name="name-on-title"]').should('not.exist');
+
+    // Should not have write-in form field.
+    cy.get('textarea[data-testid="comments-textarea"]').should('not.exist');
+
     // @todo Add tests for when search works to verify the right terms are searched for.
   });
 
   // @todo Add these tests after figuring out international address form issue.
   xit("Handles input correctly for GivingForm component", () => {
     cy.visit("/fund/bridge-forward-scholarship");
+  });
+
+  it('should render license plate funds and form', function() {
+    cy.visit("fund/cu-scholarship-license-plate-fund");
+    cy.get('h1[data-testid="fund-title"]').contains('CU Scholarship License Plate Fund');
+    cy.contains('Obtaining a CU license plate in the state of Colorado is a three step process.');
+
+    // Fund should still have the same categories listed as default funds.
+    cy.get('[data-testid="fund-campus"]').contains('CU Boulder');
+    cy.get('[data-testid="fund-allocation-code"]').contains('0122509');
+    cy.get('[data-testid="fund-interest"]').contains('Alumni Programs');
+
+    // Only one field should be on the form.
+    cy.get('input[name="name-on-title"]').should('be.visible');
+
+    // No fields from other forms.
+    cy.get('input[name="giving-amount"]').should('not.exist');
+    cy.get('textarea[data-testid="comments-textarea"]').should('not.exist');
+  });
+
+  it('should render write-in fund and form', function() {
+    cy.visit("fund/write-fund");
+    cy.get('h1[data-testid="fund-title"]').contains('Write-in Fund');
+    cy.contains('If you’d like, you can follow the steps below to write in the fund you intend your gift');
+
+    // Write-in should not have the same categories listed as default funds.
+    cy.get('[data-testid="fund-campus"]').should('not.exist');
+    cy.get('[data-testid="fund-allocation-code"]').should('not.exist');
+    cy.get('[data-testid="fund-interest"]').should('not.exist');
+
+    cy.get('input[name="giving-amount"]').should('be.visible');
+    // Extra comment section for write-in fund.
+    cy.get('textarea[data-testid="comments-textarea"]').should('be.visible');
+
+    // No license plate form field..
+    cy.get('input[name="name-on-title"]').should('not.exist');
   });
 });
