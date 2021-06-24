9b3fa6c6ca87a61138f651ff569207ae58c981dd
Alex Finnarn <alex.finnarn@gmail.com>
2021-06-24 13:59:11 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,9 @@
-module.exports = {};
+module.exports = {
+  eslint: {
+    // Warning: Vercel pretentiously added eslint runs during the build
+    // phase, but implmented it poorly. So now they make you add this line
+    // even though your CI lint check passes...maybe you should stop using
+    // Node.js entirely...just a thought.
+    ignoreDuringBuilds: true,
+  },
+}
